a7fc2d8aeee765dab9080e5c9c7cfbf7d5cc419e
DrTheDraGNaK <jean-michel.giverne@edu.devinci.fr>
2023-10-12 11:20:15 +0200

    [add] light

--- Nocmons/Assets/Scripts/ShadowEvent.cs
+++ Nocmons/Assets/Scripts/ShadowEvent.cs
@@ -2,13 +2,21 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class ShadowEvent : Event
 {
     [SerializeField] private BearReference bearRef;
+    [SerializeField] private Transform[] spawnPoints; 
+    [SerializeField] private GameObject objectToSpawn; 
+    [SerializeField] private float minSpawnTime = 2.0f; 
+    [SerializeField] private float maxSpawnTime = 5.0f; 
     private void Start()
     {
+        StartCoroutine(SpawnObject());
+
         //Take event that we need;
+        
         BearActions actions = bearRef.Instance.GetComponent<BearActions>();
         actions.EventHandBtn += Test;
     }
@@ -17,4 +25,24 @@ public class ShadowEvent : Event
     {
         Debug.Log("Pressed P");
     }
+    
+    
+    private IEnumerator SpawnObject()
+    {
+        int randomSpawnIndex = Random.Range(0, spawnPoints.Length);
+        Transform spawnPoint = spawnPoints[randomSpawnIndex];
+        
+        
+        
+        Instantiate(objectToSpawn, spawnPoint.position, spawnPoint.rotation);
+        //ajouter un bruit pour savoir qu'une ombre a été instancié
+        
+        
+        
+        float randomSpawnTime = Random.Range(minSpawnTime, maxSpawnTime);
+        yield return new WaitForSeconds(randomSpawnTime);
+
+        StartCoroutine((SpawnObject()));
+    }
+    
 }
