e18f34e6c560608ef8ba8f6fe5cb1611d5d4c8fd
Julien <julien.aberton@edu.devinci.fr>
2023-10-12 11:56:56 +0200

    update light

--- Nocmons/Assets/Scripts/LightSystem.cs
+++ Nocmons/Assets/Scripts/LightSystem.cs
@@ -1,18 +1,68 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.ShaderGraph;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class LightSystem : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private EventParameter eventParameter;
+    [SerializeField] private BearReference _bearReference;
+    
+    private int _maxNumberOfBattery;
+    private int _currentBattery;
+    public int CurrentBattery => _currentBattery;
+    private float _timeToRecharge;
+    private int _batteryToRecharge;
+
+    [SerializeField] private GameObject _light;
+    private bool usingLight;
+    private float _lightDuration;
+    private bool isRecharging;
+    private void Start()
     {
-        
+        _bearReference.Instance.GetComponent<BearActions>().EventHandBtn += HandButtonPressed;
+        _maxNumberOfBattery = eventParameter.lightNumberOfBattery;
+        _timeToRecharge = eventParameter.lightTimeBeforeRecharging;
+        _lightDuration = eventParameter.lightOnTime;
+        _currentBattery = _maxNumberOfBattery;
+    }
+    void HandButtonPressed()
+    {
+        UseLight();
     }
 
-    // Update is called once per frame
-    void Update()
+    void UseLight()
     {
-        
+        if (_currentBattery > 0 && !usingLight)
+        {
+            _currentBattery--;
+
+            StartCoroutine(LightDuration(_lightDuration));
+            Recharge();
+        }
+    }
+    void Recharge()
+    {
+        StartCoroutine(RechargeBattery(_timeToRecharge));
+        IEnumerator RechargeBattery(float time)
+        {
+            yield return new WaitForSeconds(time);
+            _currentBattery++;
+            _batteryToRecharge--;
+            isRecharging = false;
+            Recharge();
+        }   
+    }
+    
+    
+    private IEnumerator LightDuration(float time)
+    {
+        usingLight = true;
+        _light.SetActive(true);
+        yield return new WaitForSeconds(time);
+        _light.SetActive(false);
+        usingLight = false;
     }
 }
