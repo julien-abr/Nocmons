afd1bcac9c814cacb7a5f404708bcc3bae9cbf08
Jean-Michel <jean-michel.giverne@edu.devinci.fr>
2023-10-13 11:02:07 +0200

    [add] distance % travel by the enemy

--- Nocmons/Assets/ShadowMovementV2.cs
+++ Nocmons/Assets/ShadowMovementV2.cs
@@ -6,25 +6,52 @@ using UnityEngine.Serialization;
 
 public class ShadowMovementV2 : MonoBehaviour
 {
-     [SerializeField] private Transform _enemyTarget;
-     public  int _enemySpeed;
-     public int _actualEnemySpeed;
+    [SerializeField] private Transform _enemyTarget;
+    public int _enemySpeed;
+    public int _actualEnemySpeed;
+    private Vector3 _startPosition;
+    private float _totalDistance;
+    
+    [SerializeField] private bool numeroUno;
+    [SerializeField] private bool numeroDos;
+    [SerializeField] private bool numeroTres;
+    [SerializeField] private bool numeroFinal;
+    
 
-     private void Start()
-     {
-         _actualEnemySpeed = _enemySpeed;
-     }
-
-     void Update()
+    private void Start()
     {
-        if(_enemyTarget != null)
+        _actualEnemySpeed = _enemySpeed;
+        _startPosition = transform.position;
+        _totalDistance = Vector3.Distance(_startPosition, _enemyTarget.position);
+    }
+
+    void Update()
+    {
+        if (_enemyTarget != null)
         {
             transform.position = Vector3.MoveTowards(transform.position, _enemyTarget.position, _actualEnemySpeed * Time.deltaTime);
+        
+            float distanceCovered = Vector3.Distance(_startPosition, transform.position);
+
+            float percentageCovered = (distanceCovered / _totalDistance) * 100;
+
+            if (percentageCovered >= 25)
+            {
+                numeroUno = true;
+            }
+            if (percentageCovered >= 50)
+            {
+                numeroDos = true;
+            }
+            if (percentageCovered >= 75)
+            {
+                numeroTres = true;
+            }
+            if (percentageCovered >= 100)
+            {
+                numeroFinal = true;
+            }
         }
-        
-        
-        
-        
     }
     
     
