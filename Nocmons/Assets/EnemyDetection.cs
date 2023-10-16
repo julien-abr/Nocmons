43449bd5c29804be316a96e2d73f4746dc724812
DrTheDraGNaK <jean-michel.giverne@edu.devinci.fr>
2023-10-16 10:07:14 +0200

    [add] damage with pallier

--- Nocmons/Assets/EnemyDetection.cs
+++ Nocmons/Assets/EnemyDetection.cs
@@ -9,17 +9,25 @@ public class EnemyDetection : MonoBehaviour
     [SerializeField] private Camera camera; 
     
     private GameObject objectToDetect; 
-    [SerializeField] private float detectionDistance = 10f;
     [SerializeField] private float timeForFear;
     private bool isCoroutineRunning = false;
 
 
+    [SerializeField] private BearReference bearRef;
+    private BearState _bearState;
+    [SerializeField] private EventParameter _eventParameter;
     public void AddShadow(GameObject shadow)
     {
         Debug.Log(shadow.name);
         objectToDetect = shadow;
         _shadowMovement = objectToDetect.GetComponent<ShadowMovementV2>();
     }
+
+    private void Start()
+    {
+        _bearState = bearRef.Instance.GetComponent<BearState>();
+    }
+
     private void Update()
     {
         if (objectToDetect == null || camera == null)
@@ -27,6 +35,10 @@ public class EnemyDetection : MonoBehaviour
             return;
         }
 
+        if (_shadowMovement.distanceState == ShadowMovementV2.distance.isFinish)
+        {
+            Debug.Log("100");
+        }
         Vector3 screenPoint = camera.WorldToScreenPoint(objectToDetect.transform.position);
 
         if (screenPoint.z > 0 && screenPoint.x > 0 && screenPoint.x < Screen.width && screenPoint.y > 0 && screenPoint.y < Screen.height)
@@ -57,7 +69,21 @@ public class EnemyDetection : MonoBehaviour
 
         if (_shadowMovement._actualEnemySpeed == 0)
         {
-            Debug.Log("take fear damage");
+            switch (_shadowMovement.distanceState)
+            {
+                case ShadowMovementV2.distance.none :
+                    _bearState.TakeFear(_eventParameter.shadowFearPercentPoint0);
+                    break;
+                case ShadowMovementV2.distance.twentyFive : 
+                    _bearState.TakeFear(_eventParameter.shadowFearPercentPoint0);
+                    break;
+                case ShadowMovementV2.distance.fifty :
+                    _bearState.TakeFear(_eventParameter.shadowFearPercentPoint1);
+                    break;
+                case ShadowMovementV2.distance.seventyFive : 
+                    _bearState.TakeFear(_eventParameter.shadowFearPercentPoint2);
+                    break;
+            }
             
         }
         else
