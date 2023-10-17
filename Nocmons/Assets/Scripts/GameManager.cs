be2890a8e455c92bdf5decc688747d568ad0251a
DrTheDraGNaK <jean-michel.giverne@edu.devinci.fr>
2023-10-17 10:18:54 +0200

    [update] at ui scene at begining

--- Nocmons/Assets/Scripts/GameManager.cs
+++ Nocmons/Assets/Scripts/GameManager.cs
@@ -1,13 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     [SerializeField] private EventManager eventManager;
+
     void Start()
     {
         eventManager.Init();
+        SceneManager.LoadScene("UI", LoadSceneMode.Additive);
     }
     
 }
